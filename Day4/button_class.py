9908aadded610c96bca8fecfc537587cc6aea20e
Sean-McLeod <60712857+Sean-McLeod@users.noreply.github.com>
2021-06-17 05:54:38 -0400

    Update button_class.py

--- Day4/button_class.py
+++ Day4/button_class.py
@@ -8,7 +8,7 @@ import pygame
 
 
 class ButtonClass:
-    def __init__(self, color, position_x, position_y, width, height, text=''):
+    def __init__(self, color, position_x, position_y, width, height, text=""):
         self.color = color
         self.position_x = position_x
         self.position_y = position_y
@@ -19,16 +19,36 @@ class ButtonClass:
     def draw_button(self, screen, text_size, outline=None):
         # draw button
         if outline:
-            pygame.draw.rect(screen, outline, (self.position_x - 2, self.position_y - 2, self.width + 4, self.height + 4), 0)
+            pygame.draw.rect(
+                screen,
+                outline,
+                (
+                    self.position_x - 2,
+                    self.position_y - 2,
+                    self.width + 4,
+                    self.height + 4,
+                ),
+                0,
+            )
 
-        pygame.draw.rect(screen, self.color, (self.position_x, self.position_y, self.width, self.height), 0)
+        pygame.draw.rect(
+            screen,
+            self.color,
+            (self.position_x, self.position_y, self.width, self.height),
+            0,
+        )
 
-        if self.text != '':
+        if self.text != "":
             # draw text
             font = pygame.font.SysFont("comicsansms", text_size)
             text = font.render(self.text, True, (0, 0, 0))
-            screen.blit(text, (
-            self.position_x + (self.width / 2 - text.get_width() / 2), self.position_y + (self.height / 2 - text.get_height() / 2)))
+            screen.blit(
+                text,
+                (
+                    self.position_x + (self.width / 2 - text.get_width() / 2),
+                    self.position_y + (self.height / 2 - text.get_height() / 2),
+                ),
+            )
 
     def is_over(self, mouse_pos):
         # check if the mouse is over the button
