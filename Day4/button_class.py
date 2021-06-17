01095d5ba0f25c06d05b076c460ecdc8b3810193
Sean-McLeod <60712857+Sean-McLeod@users.noreply.github.com>
2021-06-17 05:43:52 -0400

    Update button_class.py

--- Day4/button_class.py
+++ Day4/button_class.py
@@ -9,31 +9,31 @@ import pygame
 
 class ButtonClass:
     def __init__(self, color, position_x, position_y, width, height, text=''):
-        self._color = color
-        self._position_x = position_x
-        self._position_y = position_y
-        self._width = width
-        self._height = height
-        self._text = text
+        self.color = color
+        self.position_x = position_x
+        self.position_y = position_y
+        self.width = width
+        self.height = height
+        self.text = text
 
     def draw_button(self, screen, text_size, outline=None):
         # draw button
         if outline:
-            pygame.draw.rect(screen, outline, (self._position_x - 2, self._position_y - 2, self._width + 4, self._height + 4), 0)
+            pygame.draw.rect(screen, outline, (self.position_x - 2, self.position_y - 2, self.width + 4, self.height + 4), 0)
 
-        pygame.draw.rect(screen, self._color, (self._position_x, self._position_y, self._width, self._height), 0)
+        pygame.draw.rect(screen, self.color, (self.position_x, self.position_y, self.width, self.height), 0)
 
-        if self._text != '':
+        if self.text != '':
             # draw text
             font = pygame.font.SysFont("comicsansms", text_size)
-            text = font.render(self._text, True, (0, 0, 0))
+            text = font.render(self.text, True, (0, 0, 0))
             screen.blit(text, (
-            self._position_x + (self._width / 2 - text.get_width() / 2), self._position_y + (self._height / 2 - text.get_height() / 2)))
+            self.position_x + (self.width / 2 - text.get_width() / 2), self.position_y + (self.height / 2 - text.get_height() / 2)))
 
     def is_over(self, mouse_pos):
         # check if the mouse is over the button
-        if self._position_x < mouse_pos[0] < self._position_x + self._width:
-            if self._position_y < mouse_pos[1] < self._position_y + self._height:
+        if self.position_x < mouse_pos[0] < self.position_x + self.width:
+            if self.position_y < mouse_pos[1] < self.position_y + self.height:
                 return True
 
         return False
