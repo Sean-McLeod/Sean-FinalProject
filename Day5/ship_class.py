e0ef9ea35549a9d940af87211b6b9ce3e24f5c23
Sean-McLeod <60712857+Sean-McLeod@users.noreply.github.com>
2021-06-17 07:33:58 -0400

    Update ship_class.py

--- Day5/ship_class.py
+++ Day5/ship_class.py
@@ -12,16 +12,3 @@ from monsters import Monsters
 class ShipClass(Monsters):
     def __init__(self, sprite, sprite_x, sprite_y, x_speed, y_speed, screen):
         super().__init__(sprite, sprite_x, sprite_y, x_speed, y_speed, screen)
-
-    def ship_move(self):
-        super().sprite_move(super().get_x_speed(), super().get_y_speed())
-        if self.keep_inside_screen():
-            super().set_x_speed(super().get_x_speed() * -1)
-
-    def create_bullet(self):
-        return BulletClass(
-            super().get_rect().x + constants.SHIP_MIDDLE_X,
-            super().get_rect().y + constants.SHIP_MIDDLE_Y,
-            constants.BULLET_X_SPEED,
-            constants.BULLET_Y_SPEED,
-        )
