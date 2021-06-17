d5a8cd60db60942c585f53e4bdb30109974049e4
Sean-McLeod <60712857+Sean-McLeod@users.noreply.github.com>
2021-06-17 05:45:08 -0400

    Update maps.py

--- Day4/maps.py
+++ Day4/maps.py
@@ -2,9 +2,11 @@
 
 # Created by Sean McLeod
 # Created on June 2021
-# This is the maps class
+# This is the map class
 
 import pygame
+
+import constants
 
 
 class Maps:
@@ -13,56 +15,50 @@ class Maps:
         self._sprite = sprite
         self._width = self._sprite.get_width()
         self._height = self._sprite.get_height()
-        self.maps = []
-        self.level = level
-        self.tile_x_distance = tile_x_distance
-        self.tile_y_distance = tile_y_distance
-        self.angle = 90
-        self.rotated_sprite = pygame.transform.rotate(self._sprite, self.angle)
-        self.rotated_width = self.rotated_sprite.get_width()
-        self.rotated_height = self.rotated_sprite.get_height()
-        self.new_rect = self._sprite.get_rect()
-        self.reduce_distance = self.tile_y_distance / 2
+        self._maps = []
+        self._level = level
+        self._tile_x_distance = tile_x_distance
+        self._tile_y_distance = tile_y_distance
+        self._rotated_sprite = pygame.transform.rotate(self._sprite, constants.RIGHT_ANGLE)
+        self._rotated_width = self._rotated_sprite.get_width()
+        self._rotated_height = self._rotated_sprite.get_height()
+        self._new_rect = self._sprite.get_rect()
+        self._reduce_distance = self._tile_y_distance/2
         self._game_scene_one_map = """
 
 
-
+    
 
 
 wwwwwwwwww  ww""".splitlines()
 
-        self._game_scene_two_map = """ ff
+        self._game_scene_two_map = """ 
  rr
- fwww w
+ rwww w
  r     r
 wwwwwww
      r 
  r   r 
  wwww ww
-
+ 
 wwwww wwww
 """.splitlines()
-        self.maps.append(self._game_scene_one_map)
-        self.maps.append(self._game_scene_two_map)
+        self._maps.append(self._game_scene_one_map)
+        self._maps.append(self._game_scene_two_map)
 
     def build_map(self, sprite2):
-        for nth_line, line in enumerate(self.maps[self.level - 1]):
+        for nth_line, line in enumerate(self._maps[self._level - 1]):
             for nth_character, character in enumerate(line):
                 if character == "w":
-                    self._screen.blit(self._sprite,
-                                      (nth_character * self.tile_x_distance, nth_line * self.tile_y_distance))
-                    self.new_rect = pygame.Rect(nth_character * self.tile_x_distance, nth_line * self.tile_y_distance,
-                                                self._width, self._height)
+                    self._screen.blit(self._sprite, (nth_character * self._tile_x_distance, nth_line * self._tile_y_distance))
+                    new_rect = pygame.Rect(nth_character * self._tile_x_distance, nth_line * self._tile_y_distance, self._width, self._height)
                     # pygame.draw.rect(self._screen, (0, 0, 255), self.new_rect)
                     # check collision
-                    if self.new_rect.colliderect(sprite2):
+                    if new_rect.colliderect(sprite2):
                         print("collided with map")
                 elif character == "r":
-                    self.rotated_rect = pygame.Rect(nth_character * self.tile_x_distance,
-                                                    nth_line * self.tile_y_distance - self.reduce_distance,
-                                                    self.rotated_width, self.rotated_height)
-                    self._screen.blit(self.rotated_sprite, (
-                    nth_character * self.tile_x_distance, nth_line * self.tile_y_distance - self.reduce_distance))
+                    rotated_rect = pygame.Rect(nth_character * self._tile_x_distance, nth_line * self._tile_y_distance - self._reduce_distance, self._rotated_width, self._rotated_height)
+                    self._screen.blit(self._rotated_sprite, (nth_character * self._tile_x_distance, nth_line * self._tile_y_distance - self._reduce_distance))
                     # check collision
-                    if self.rotated_rect.colliderect(sprite2):
+                    if rotated_rect.colliderect(sprite2):
                         print("collided with map")
