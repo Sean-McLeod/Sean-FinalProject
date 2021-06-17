70035a5b281986811fa91b49ab9ea9f8b4893b73
Sean-McLeod <60712857+Sean-McLeod@users.noreply.github.com>
2021-06-16 20:34:54 -0400

    Update PrisonerClass.py

--- Day2/PrisonerClass.py
+++ Day2/PrisonerClass.py
@@ -6,14 +6,16 @@ from Sprites import Sprites
 class PrisonerClass(Sprites):
     def __init__(self, sprite, sprite_x, sprite_y, x_speed, y_speed, screen):
         super().__init__(sprite, sprite_x, sprite_y, x_speed, y_speed, screen)
-        self.prisoners = [pygame.image.load("Sprites/prisoners/prisoner2.png"),
-                          pygame.image.load("Sprites/prisoners/prisoner3.png"), pygame.image.load(
-                "Sprites/prisoners/prisoner4.png"),
-                          pygame.image.load("Sprites/prisoners/prisoner5.png"), pygame.image.load(
-                "Sprites/prisoners/prisoner6.png"),
-                          pygame.image.load("Sprites/prisoners/prisoner7.png"), pygame.image.load(
-                "Sprites/prisoners/prisoner8.png"),
-                          pygame.image.load("Sprites/prisoners/prisoner9.png")]
+        self.prisoners = [
+            pygame.image.load("Sprites/prisoners/prisoner2.png"),
+            pygame.image.load("Sprites/prisoners/prisoner3.png"),
+            pygame.image.load("Sprites/prisoners/prisoner4.png"),
+            pygame.image.load("Sprites/prisoners/prisoner5.png"),
+            pygame.image.load("Sprites/prisoners/prisoner6.png"),
+            pygame.image.load("Sprites/prisoners/prisoner7.png"),
+            pygame.image.load("Sprites/prisoners/prisoner8.png"),
+            pygame.image.load("Sprites/prisoners/prisoner9.png"),
+        ]
         self.is_animating = False
         self.looking_right = False
         self.prisoner_x_change = 0
@@ -30,7 +32,9 @@ class PrisonerClass(Sprites):
         if not self.looking_right:
             self.flip_sprite()
 
-    def prisoner_move(self, key_is_down, key_left, key_right, key_up, key_down, key_is_up):
+    def prisoner_move(
+        self, key_is_down, key_left, key_right, key_up, key_down, key_is_up
+    ):
         if key_is_down:
             if key_left:
                 self.is_animating = True
