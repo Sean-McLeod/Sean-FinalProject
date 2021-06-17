4ae4e09742e2992ba090601b3e9b01b2d0c640d3
Sean-McLeod <60712857+Sean-McLeod@users.noreply.github.com>
2021-06-17 11:06:56 -0400

    Update bullets.py

--- Day6/bullets.py
+++ Day6/bullets.py
@@ -1,1 +1,24 @@
+import pygame
 
+import constants
+
+
+class BulletClass(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y, x_speed, y_speed):
+        super().__init__()
+        self.image = pygame.transform.rotate(pygame.image.load(constants.BULLET_IMAGE), constants.ROTATE_THREE_TIMES)
+        self._pos_y = pos_y
+        self._pos_x = pos_x
+        self._x_speed = x_speed
+        self._y_speed = y_speed
+        self.rect = pygame.Rect(self._pos_x, self._pos_y, self.image.get_width(), self.image.get_height())
+
+    def update(self, to_shoot):
+        self.rect.y += self._y_speed
+
+        if self.rect.y >= constants.SCREEN_HEIGHT:
+            # if the bullet passes the screen, delete the bullet(for memory).
+            self.kill()
+
+        if self.rect.colliderect(to_shoot):
+            print("SHOT")
