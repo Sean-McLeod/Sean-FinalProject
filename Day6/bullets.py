74ad280e7be23583d85015f6406b919721b93d1d
Sean-McLeod <60712857+Sean-McLeod@users.noreply.github.com>
2021-06-17 11:14:26 -0400

    Update bullets.py

--- Day6/bullets.py
+++ Day6/bullets.py
@@ -1,17 +1,21 @@
-import pygame
 
 import constants
+import pygame
 
 
 class BulletClass(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y, x_speed, y_speed):
         super().__init__()
-        self.image = pygame.transform.rotate(pygame.image.load(constants.BULLET_IMAGE), constants.ROTATE_THREE_TIMES)
+        self.image = pygame.transform.rotate(
+            pygame.image.load(constants.BULLET_IMAGE), constants.ROTATE_THREE_TIMES
+        )
         self._pos_y = pos_y
         self._pos_x = pos_x
         self._x_speed = x_speed
         self._y_speed = y_speed
-        self.rect = pygame.Rect(self._pos_x, self._pos_y, self.image.get_width(), self.image.get_height())
+        self.rect = pygame.Rect(
+            self._pos_x, self._pos_y, self.image.get_width(), self.image.get_height()
+        )
 
     def update(self, to_shoot):
         self.rect.y += self._y_speed
